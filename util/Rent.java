8a89ae9476edee901abd66c1b754f8ae567f7a70
Diego Florentino <claudinodiego98@gmail.com>
2026-03-09 23:18:16 -0300

    Add files via upload

---
+++ util/Rent.java
@@ -0,0 +1,35 @@
+package util;
+
+public class Rent {
+    private String nomes;
+    private String email;
+
+    public Rent (){
+
+    }
+
+    public Rent(String nomes, String email) {
+        this.nomes = nomes;
+        this.email = email;
+    }
+
+    public String getNomes() {
+        return nomes;
+    }
+
+    public void setNomes(String nomes) {
+        this.nomes = nomes;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String toString(){
+        return nomes + ", " + email;
+    }
+}
